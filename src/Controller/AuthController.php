674d43576aa650af8c803c6a503b147587e02748
sizalel <teolis.rainyday@gmail.com>
2020-06-05 15:21:07 +0200

    routes

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\JsonResponse;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use App\Entity\User;
+    use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AuthController extends AbstractController
+{
+	public function register(Request $request, UserPasswordEncoderInterface $encoder)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$username  = json_decode($request->getContent())->username;
+		$password = json_decode($request->getContent())->password;
+
+		$user = new User();
+		$user->setUsername (($username));
+		$user->setPassword($encoder->encodePassword($user, $password));
+		$user->setRoles(['ROLE_USER']);
+		$em->persist($user);
+		$em->flush();
+
+		return new Response(sprintf('User %s successfully created', $user->getUsername()));
+	}
+
+	public function api()
+	{
+		return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
+	}
+
+	public function getCompleteUser() {
+		return $this->json($this->getUser());
+	}
+}
---
+++ src/Entity/Raviole.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\RavioleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=RavioleRepository::class)
+ */
+class Raviole
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=2000)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, scale=2)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): self
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+}
